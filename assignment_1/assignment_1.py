37e2bc630f2a6a2b7cb5fe27477aefb38cd5a2fb
nitinkumar7273 <98381694+nitinkumar7273@users.noreply.github.com>
2022-12-21 00:18:46 +0530

    updated

--- assignment_1/assignment_1.py
+++ assignment_1/assignment_1.py
@@ -1,21 +1,9 @@
 # Assignment:
 # You have to read the below CSV file and convert the data in json format given in the sample file
 
-import csv
 import pandas as pd
-import json
 
 df = pd.read_csv(r'mvf_user.csv')
-df.to_json(r'converted_mvf_user.json')
-
-read_json = open(
-    '/Users/nitinkumarpawar/Developer/Arrk/Training/assignments/assignment_1/converted_mvf_user.json', 'r')
-
-json_data = read_json.read()
-json_obj = json.loads(str(json_data))
-
-json_to_dic = json.load(json_data)
-print(json_to_dic)
-print(type(json_to_dic))
-
-read_json.close()
+convert_json = df.to_json(r'converted_mvf_user.json')
+json_output = df.to_json(convert_json, indent=1, orient='records')
+print(json_output)
